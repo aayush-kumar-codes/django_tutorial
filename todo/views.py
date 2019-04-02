dcda2197476c604a79b7e609504beaf7249e238b
etech <email@gmail.com>
2019-04-02 15:03:58 +0530

    tutorial

--- todo/views.py
+++ todo/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
-
-# Create your views here.
-
+from todo.models import Todo
+import json
+from django.utils import timezone
 
 def index(request):
     return HttpResponse("Hello, world!")
 
-@csrf_exempt    
+
+@csrf_exempt
 def addTodo(request):
     if request.method == "POST":
-        return HttpResponse("OK")
+        req = json.loads(request.body)
+        print(req)
+        todo = Todo(task=req["task"], created_at=timezone.now())
+        todo.save()
+
+        return HttpResponse(todo.id)
     else:
         return HttpResponseNotAllowed("POST")
