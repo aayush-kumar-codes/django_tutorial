c3e3ae96dfb23042443d1460ac8fdcc15f1c7c8d
etech <email@gmail.com>
2019-04-03 15:55:53 +0530

    tutorial

--- todo/models.py
+++ todo/models.py
@@ -6,7 +6,13 @@ import datetime
 
 
 
+class User(models.Model):
+    username = models.CharField(max_length=255,unique=True)
+    password = models.CharField(max_length=255)
+
+
 class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.CharField(max_length=255)
     due_date = models.DateField(null=True)
     description = models.TextField(default="")
