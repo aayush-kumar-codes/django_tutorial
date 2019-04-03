004d8f730c1f2c7cd8d0ae1c631251c7108e1c24
etech <email@gmail.com>
2019-04-03 15:48:28 +0530

    tutorial

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 # Create your models here.
+import datetime
+
+
 
 
 class Todo(models.Model):
-    task = models.CharField(max_length=200)
-    created_at = models.DateTimeField("date published")
+    task = models.CharField(max_length=255)
+    due_date = models.DateField(null=True)
+    description = models.TextField(default="")
+    is_complete = models.BooleanField(default=False)
+    complete_percentage  = models.DecimalField(default=0,max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
