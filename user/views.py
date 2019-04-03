9563fb5fa6e22871aaef418d3f710eda473a6cd5
etech <email@gmail.com>
2019-04-03 17:49:50 +0530

    tutorial

--- user/views.py
+++ user/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
 
+import json
+from passlib.hash import pbkdf2_sha256
+
+from user.models import User
 
 @csrf_exempt
 def login(request):
@@ -9,4 +13,17 @@ def login(request):
 
 @csrf_exempt
 def register(request):
-    return HttpResponse("")
+
+    if not request.method == "POST":
+        return HttpResponseNotAllowed("POST")
+
+    req = json.loads(request.body)
+
+    # add validations here if need. for now i am skipping that
+
+    username = req["username"]
+    password = req["password"]
+
+    user = User(username=username,password=pbkdf2_sha256.hash(password))
+    user.save()
+    return HttpResponse(user.id)
